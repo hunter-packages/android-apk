501c7fe53c2291469f6cf8ba7f6469d8eafb1369
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-07-28 17:12:04 +0300

    Update year

--- AndroidApk.cmake
+++ AndroidApk.cmake
@@ -3,7 +3,7 @@
 #*    Android apk tools
 #*
 #*  Copyright (C) 2002-2013 The PixelLight Team (http://www.pixellight.org/)
-#*  Copyright (C) 2015 Ruslan Baratov
+#*  Copyright (C) 2015-2016 Ruslan Baratov
 #*  Copyright (C) 2015 David Hirvonen
 #*
 #*  This file is part of PixelLight.
